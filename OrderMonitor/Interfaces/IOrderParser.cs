00a11cdf4ab1dff171f778618711a4ad70598d17
tanrajp01 <tanrajp01@gmail.com>
2018-07-24 22:43:40 +0100

    Refactor the csv parser and xml writer into their own files

---
+++ OrderMonitor/Interfaces/IOrderParser.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Globalization;
+using System.Xml;
+using CsvHelper;
+using CsvHelper.Configuration;
+using OrderMonitor.RowPart;
+
+namespace OrderMonitor.Interfaces
+{
+    public interface IOrderParser
+    {
+        IList<Order> ReadOrders(string fileName);
+    }
+
+
+    public class CSVOrderParse : IOrderParser
+    {
+        public IList<Order> ReadOrders(string fileName)
+        {
+            var orders = new List<Order>();
+
+            using (StreamReader streamReader = File.OpenText(fileName))
+            {
+                var config = new Configuration();
+                config.HasHeaderRecord = true;
+                config.PrepareHeaderForMatch = header => header?.Trim();
+                config.RegisterClassMap<RowMap>();
+
+                var csv = new CsvReader(streamReader, config);
+
+                var currentRow = new Row();
+                var currentOrder = new Order();
+
+                while (csv.Read())
+                {
+                    var row = csv.GetRecord<Row>();
+                    if (currentRow == null || currentRow.OrderNo != row.OrderNo)
+                    {
+                        var order = row.CreateOrder();
+                        var consignment = row.CreateConsignment();
+                        var parcel = row.CreateParcel();
+                        var item = row.CreateItem();
+
+                        parcel.Items.Add(item);
+                        consignment.Parcels.Add(parcel);
+                        order.Consignments.Add(consignment);
+
+                        currentRow = row;
+                        currentOrder = order;
+                        orders.Add(order);
+                    }
+                    else
+                    {
+                        if (currentRow.ConsignmentNo != row.ConsignmentNo)
+                        {
+                            var consignment = row.CreateConsignment();
+                            var parcel = row.CreateParcel();
+                            var item = row.CreateItem();
+
+                            parcel.Items.Add(item);
+                            consignment.Parcels.Add(parcel);
+
+                            currentOrder.Consignments.Add(consignment);
+                        }
+                        else
+                        {
+                            var parcel = row.CreateParcel();
+                            var item = row.CreateItem();
+
+                            parcel.Items.Add(item);
+
+                            var toupdate = currentOrder.Consignments.Where(x => x.ConsignmentNo == row.ConsignmentNo).Single();
+                            toupdate.Parcels.Add(parcel);
+                        }
+                    }
+                }
+
+            }
+
+            return orders;
+        }
+    }
+}
---
+++ OrderMonitor/Interfaces/IXmlOrderCreator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using OrderMonitor;
+
+namespace OrderMonitor.Interfaces
+{
+    public interface IOrderOutputter
+    {
+        void CreateDocument(IList<Order> orders);
+    }
+
+    public class XmlOrderOutputter : IOrderOutputter
+    {
+        private string directory;
+        private string fileName;
+
+        public XmlOrderOutputter(string directory, string fileName)
+        {
+            this.directory = directory;
+            this.fileName = fileName;
+        }
+
+        public void CreateDocument(IList<Order> orders)
+        {
+            using (XmlWriter writer = XmlWriter.Create($"{directory}\\{fileName}.xml"))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Orders");
+
+                foreach (var order in orders)
+                {
+                    order.CreateNode(writer);
+                }
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+        }
+    }
+}
